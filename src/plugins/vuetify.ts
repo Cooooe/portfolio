7fa08a0f5c651a3e9f5bed266452d039a63ac39a
KangHyungwon <hyungwon.kang@huesik.co.kr>
2020-02-05 17:06:44 +0900

    update header

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -4,4 +4,14 @@ import Vuetify from 'vuetify/lib'
 Vue.use(Vuetify)
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: '#6A6A6B',
+        secondary: '#686E6F',
+        accent: '#F6F6F6',
+        error: '#86606B'
+      }
+    }
+  }
 })
